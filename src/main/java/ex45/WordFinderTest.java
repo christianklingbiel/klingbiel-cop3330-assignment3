9a952cd63d349574dd4e35a8d482685443305fed
Christian Klingbiel <christianklingbiel991@gmail.com>
2021-06-26 11:14:58 -0400

    push

--- src/main/java/ex45/WordFinderTest.java
+++ src/main/java/ex45/WordFinderTest.java
@@ -1,4 +1,18 @@
 package ex45;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WordFinderTest {
+    void sort() throws Exception{
+        List<String> input = new ArrayList<>();
+        input.add("utilize utilize utilize");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("use use use");
+
+        assertEquals(WordFinder.replaceWord(input),expected);
+    }
 }
--- src/main/java/ex46/Test.java
+++ src/main/java/ex46/Test.java
@@ -1,4 +1,8 @@
 package ex46;
 
 public class Test {
+    void sort() throws Exception{
+        //WordTable.build();
+        //WordTable.print();
+    }
 }
